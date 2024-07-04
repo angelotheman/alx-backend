db95cb6054bdf12ac9320efcbf9b4fb0430dbc84
Kwabena Twumasi <ktwum721@gmail.com>
2024-07-04 01:25:48 +0000

    ✅ Simple Pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Finding the index range with pagination
+    """
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+
+    return start_idx, end_idx
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns a particular page
+        """
+        assert isinstance(page, int) and page > 0,
+        "Page must be an integer greater than 0."
+
+        assert isinstance(page_size, int) and page_size > 0,
+        "Page size must be an integer greater than 0."
+
+        start_idx, end_idx = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_idx >= len(dataset):
+            return []
+
+        return dataset[start_idx:end_idx]
